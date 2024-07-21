ec7eb565503f108523f85a5e7a04f27d1568cdf6
TetianaBukhenko <tania04bukhenko@gmail.com>
2024-07-22 02:08:07 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,27 @@
+"use strict";
+function waitFor(element, eventName) {
+    return new Promise((resolve, reject)=>{
+        element.addEventListener(eventName, (e)=>{
+            if (e.target === element) resolve(`It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`);
+        });
+    });
+}
+const printMessage = (message)=>{
+    const element = document.createElement("div");
+    element.classList.add("message");
+    element.innerText = message;
+    document.body.insertAdjacentElement("beforeend", element);
+};
+const loginField = document.getElementById("login");
+const passwordField = document.getElementById("password");
+const button = document.getElementById("submit");
+waitFor(loginField, "click").then(printMessage);
+waitFor(passwordField, "click").then(printMessage);
+waitFor(button, "click").then(printMessage);
+waitFor(loginField, "input").then(printMessage);
+waitFor(passwordField, "input").then(printMessage);
+waitFor(loginField, "blur").then(printMessage);
+waitFor(passwordField, "blur").then(printMessage);
+waitFor(button, "blur").then(printMessage);
+
+//# sourceMappingURL=index.f75de5e1.js.map
